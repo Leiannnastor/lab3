eb683056d2ebec1aed8bb42d61f066d3859f81a9
Lei Ann Nastor <131526238+Leiannnastor@users.noreply.github.com>
2023-11-02 12:46:10 +0800

    Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,8 +5,22 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-$routes->get('test', 'StudentController::test');
-$routes->post('save', 'StudentController::save');
-$routes->get('delete/(:any)', 'StudentController::delete/$1');
-$routes->get('update/(:any)', 'StudentController::update/$1');
+
+
+$routes->get('/manage', 'ManageController::index', ['filter' => 'admin']);
+$routes->post('/add', 'ManageController::add', ['filter' => 'admin']);
+$routes->post('/edit', 'ManageController::edit', ['filter' => 'admin']);
+$routes->get('/delete/(:num)', 'ManageController::delete/$1', ['filter' => 'admin']);
+
+
+$routes->get('/', 'ProductsController::index', );
+$routes->get('/shops', 'ProductsController::shops', ['filter' => 'Authguard']);
+$routes->get('/contact', 'ProductsController::contact', ['filter' => 'Authguard']);
+$routes->get('/singleprod/(:num)', 'ProductsController::singleprod/$1', ['filter' => 'Authguard']);
+
+
+$routes->get('/login', 'UserController::login');
+$routes->post('/loginauth', 'UserController::loginauth');
+$routes->get('/logout', 'UserController::logout');
+$routes->get('/register', 'UserController::register');
+$routes->post('/registerauth', 'UserController::registerauth');
