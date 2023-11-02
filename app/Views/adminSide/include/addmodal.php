5a81d036cf5cae79bb8b84bb5becc438d6b831d3
Lei Ann Nastor <131526238+Leiannnastor@users.noreply.github.com>
2023-11-02 13:02:50 +0800

    addmodal.php

--- app/Views/adminSide/include/addmodal.php
+++ app/Views/adminSide/include/addmodal.php
@@ -1,119 +1,48 @@
-<!DOCTYPE html>
-<html>
+<!-- add_product_modal.php -->
 
-<head>
-    <title>Product Management</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-</head>
+<div class="modal fade" id="addProductModal" tabindex="-1" aria-labelledby="addProductModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="addProductModalLabel">Add Product</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form method="POST" action="<?= base_url('add'); ?>" enctype="multipart/form-data">
+                    <div class="mb-3">
+                        <label for="productName" class="form-label">Product Name</label>
+                        <input type="text" class="form-control" id="productName" name="productName">
+                    </div>
+                    <div class="mb-3">
+                        <label for="category" class="form-label">Category</label>
+                        <input type="text" class="form-control" id="category" name="category">
+                    </div>
+                    <div class="mb-3">
+                        <label for="details" class="form-label">Details</label>
+                        <textarea class="form-control" id="details" name="details"></textarea>
+                    </div>
+                    <div class="mb-3">
+                        <label for="price" class="form-label">Price</label>
+                        <input type="text" class="form-control" id="price" name="price">
+                    </div>
+                    <div class="mb-3">
+                        <label for="availability" class="form-label">Availability</label>
+                        <select class="form-select" id="availability" name="availability">
+                            <option value="in stock">In Stock</option>
+                            <option value="out of stock">Out of Stock</option>
+                        </select>
+                    </div>
 
-<body>
-    <div class="container mt-5">
-        <!-- Include Bootstrap JavaScript and jQuery -->
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
-        <!-- Include Edit Product Modal -->
-        <?= $this->include('adminSide/include/editmodal.php') ?>
-        <?= $this->include('adminSide/include/addmodal.php') ?>
-
-        <h2>Product Management</h2>
-
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addProductModal">
-            Add Product
-        </button>
-
-        <br>
-
-        <a href="<?= base_url(''); ?>" class="nav-link text-body font-weight-bold px-0">
-            <i class="fa fa-shopping-cart me-sm-1"></i>
-            <span class="d-sm-inline d-none">Go to Shop</span>
-        </a>
-
-        <!-- List of Products -->
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Product Name</th>
-                    <th>Category</th>
-                    <th>Details</th>
-                    <th>Price</th>
-                    <th>Availability</th>
-                    <th>Image</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($products as $product): ?>
-                    <tr>
-                        <td>
-                            <?= $product['id'] ?>
-                        </td>
-                        <td>
-                            <?= $product['productname'] ?>
-                        </td>
-                        <td>
-                            <?= $product['category'] ?>
-                        </td>
-                        <td>
-                            <?= $product['details'] ?>
-                        </td>
-                        <td>
-                            <?= $product['price'] ?>
-                        </td>
-                        <td>
-                            <?= $product['availability'] ?>
-                        </td>
-                        <td><img src="<?= base_url($product['img']) ?>" width="100"></td>
-                        <td>
-                            <button type="button" class="btn btn-primary edit-product" data-toggle="modal"
-                                data-target="#editProductModal" data-id="<?= $product['id']; ?> "
-                                data-productname="<?= $product['productname']; ?> "
-                                data-category="<?= $product['category']; ?>" data-details="<?= $product['details']; ?>"
-                                data-price="<?= $product['price']; ?>" data-availability="<?= $product['availability']; ?>"
-                                data-img="<?= base_url() . $product['img']; ?>">
-                                Edit
-                            </button>
-                            <a href="<?= base_url() ?>delete/<?= $product['id'] ?>" class="btn btn-danger">Delete</a>
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
+                    <div class="mb-3">
+                        <label for="img" class="form-label">Image Upload</label>
+                        <input type="file" class="form-control" id="img" name="img">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Add Product</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
-
-
-    <script>
-        // Function to populate the edit modal
-        function populateEditModal(id, productName, category, details, price, availability, img) {
-            // Set values in the modal form fields
-            $('#editId').val(id);
-            $('#editProductName').val(productName);
-            $('#editCategory').val(category);
-            $('#editDetails').val(details);
-            $('#editPrice').val(price);
-            $('#editAvailability').val(availability);
-            $('#editImg').val('');
-
-            // Display the image
-            $('#editImagePreview').attr('src', img);
-        }
-
-        // When the edit-product button is clicked, populate the modal with data
-        $('.edit-product').on('click', function () {
-            var id = $(this).data('id');
-            var productName = $(this).data('productname');
-            var category = $(this).data('category');
-            var details = $(this).data('details');
-            var price = $(this).data('price');
-            var availability = $(this).data('availability');
-            var img = $(this).data('img');
-
-            // Call the function to populate the edit modal
-            populateEditModal(id, productName, category, details, price, availability, img);
-        });
-    </script>
-
-</body>
-
-</html>
+</div>
